d1bb469b088b5a0a3f4968b3f11bea1695759771
Rikita Roy <rikitaroy21@gmail.com>
2026-03-17 00:30:08 +0530

    edit main.jsx

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {BrowserRouter} from 'react-router'
+import { ToastContainer } from 'react-toastify'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <ToastContainer/>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
