1e7f6733f3be2243b7651bf90706128ccb94d261
Rikita Roy <rikitaroy21@gmail.com>
2026-03-17 00:29:30 +0530

    edit app.jsx file

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
-import Navbar from "./Navebar.jsx";
+import {Routes, Route} from 'react-router'
+import Navbar from './Component/Navbar'
+import Footer from './Component/Footer'
+import Dashboard from 'Pages/Dashboard.jsx'
+import Calender from 'Pages/Calender.jsx'
+import Add from 'Pages/Add.jsx'
+import Home from 'Pages/Home.jsx'
+import Login from './auth/Login.jsx'
+import Register from './auth/Register'
 
-const App = () => {
+
+function App() {
   return (
     <div>
-      <Navbar />
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register/>}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/calender" element={<Calender />}></Route>
+        <Route path="/add" element={<Add/>}></Route>
+      </Routes>
+      <Footer/>
     </div>
-  )
+  );
 }
 
 export default App
