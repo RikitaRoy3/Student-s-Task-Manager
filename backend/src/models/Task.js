8766afde17f68a10f4a13c564603ac6180e395ff
Rikita Roy <rikitaroy21@gmail.com>
2026-03-19 18:24:10 +0530

    add Task model

---
+++ backend/src/models/Task.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema({
+  TaskName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  Description: {
+    type: String,
+    trim: true,
+  },
+  Priority: {
+    type: String,
+    enum: ["Low", "Medium", "High"]
+  },
+  DueDate:{
+    type: Date,
+    required: true,
+  }
+
+}, { timestamps: true });
+
+const Task = mongoose.model("Task", TaskSchema);
+
+export default Task;
+
