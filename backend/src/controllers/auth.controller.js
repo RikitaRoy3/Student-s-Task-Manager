e1e2024e94cf41bb59eb74b7fb55ee11c4de7a76
Rikita Roy <rikitaroy21@gmail.com>
2026-03-22 00:01:15 +0530

    minor issue found

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { ENV } from "../lib/env.js";
 import { generateToken } from "../lib/generatetoken.js";
+import {  sendWelcomeEmail} from "../emails/emailHandlers.js";
 
 
 import dotenv from "dotenv";
