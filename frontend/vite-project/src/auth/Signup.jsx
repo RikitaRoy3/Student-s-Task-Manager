54c457de25821c0a6d8b9d39e264e9692d1ae0ac
Rikita Roy <rikitaroy21@gmail.com>
2026-03-22 00:19:11 +0530

    very minor ui fix

--- frontend/vite-project/src/auth/Signup.jsx
+++ frontend/vite-project/src/auth/Signup.jsx
@@ -36,7 +36,7 @@ function Signup() {
                   placeholder="Enter Your Full Name"
                   className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
                   required
-                  onChange={(e) => setFirstName(e.target.value)}
+                  onChange={(e) => setFullName(e.target.value)}
                 />
               </div>
 
