bee113825f663b5725ff71e5374cb73935548501
Rikita Roy <rikitaroy21@gmail.com>
2026-03-22 11:13:38 +0530

    modify code to pass user id(in the route) while creating a new_task

--- backend/src/routes/task.route.js
+++ backend/src/routes/task.route.js
@@ -3,6 +3,6 @@ import { new_task} from "../controllers/task.controller.js";
 
 const router = express.Router();
 
-router.post("/new_task", new_task);
+router.post("/new_task/:userId", new_task);
 
 export default router;
