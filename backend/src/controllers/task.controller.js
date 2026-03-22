fb6004818b13f46f41c6f4075281087b3e7de71f
Rikita Roy <rikitaroy21@gmail.com>
2026-03-23 00:11:08 +0530

    changes done in new task controller (to accept user id from cookies using jwt)

--- backend/src/controllers/task.controller.js
+++ backend/src/controllers/task.controller.js
@@ -1,24 +1,19 @@
 import Task from "../models/Task.js";
 import User from "../models/User.js";
 import { ENV } from "../lib/env.js";
+import jwt from "jsonwebtoken";
 
 import dotenv from "dotenv";
 dotenv.config();
 
+
+/* ===================== NEW TASK ===================== */
 export const new_task = async (req, res) => {
-  const { TaskTitle, Description, Priority, DueDate } = req.body;
-
-
   try {
-    const  userId  = req.params.userId;
+    const { TaskTitle, Description, Priority, DueDate } = req.body;
 
     if (!TaskTitle || !Description || !Priority || !DueDate) {
       return res.status(400).json({ message: "Please provide all the required fields" });
-    }
-
-    const existingUser = await User.findOne({ _id: userId });
-    if (!existingUser) {
-      return res.status(404).json({ message: "User not found" });
     }
 
     const new_task = await Task.create({
@@ -26,24 +21,24 @@ export const new_task = async (req, res) => {
       Description,
       Priority,
       DueDate,
-      user: userId
+      user: req.user._id
     });
 
     res.status(201).json({
       user: {
         message: "New Task registered successfully",
-        _id: new_task._id,
+        Task_Id: new_task._id,
         TaskTitle: new_task.TaskTitle,
         Description: new_task.Description,
         Priority: new_task.Priority,
         DueDate: new_task.DueDate,
-        user: new_task.user
+        User_Id: new_task.user
       },
     });
-    
+
   }
-    catch (error) {
-      console.error("Error in new_task controller:", error);
-      res.status(500).json({ message: "Internal server error" });
-    }
+  catch (error) {
+    console.error("Error in new_task controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
