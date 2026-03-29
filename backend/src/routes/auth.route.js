1a08e2b48e43c0b050dda3eef2d571a9e0169cb1
Rikita Roy <rikitaroy21@gmail.com>
2026-03-29 23:59:15 +0530

    add a missing import

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { signup, login } from "../controllers/auth.controller.js";
 import { checkauth } from "../middleware/auth.middleware.js";
+import { dashboard } from "../controllers/auth.controller.js";
 
 
 const router = express.Router();
