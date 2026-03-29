751a2b8bfb411cf26bf19a144718d16b66cae0de
Rikita Roy <rikitaroy21@gmail.com>
2026-03-29 23:29:53 +0530

    edit app.jsx

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -41,17 +41,20 @@ import Profile from "./auth/Profile.jsx";
 import Login from "./auth/Login.jsx";
 import Signup from "./auth/Signup.jsx";
 import Taskslist from "./pages/Taskslist.jsx";
+import Home from "./Component/Home.jsx";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home/>
       <Routes>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
         <Route path="/tasks-list" element={<Taskslist />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/Signup" element={<Signup />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
+        
         <Route path="/add" element={<Add />}></Route>
       </Routes>
     </div>
