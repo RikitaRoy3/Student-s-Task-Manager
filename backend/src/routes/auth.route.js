ef278e31def9d93ffda56171c6e0a5b5e1697d24
Rikita Roy <rikitaroy21@gmail.com>
2026-03-31 15:52:15 +0530

    add tasklist route in backend

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,6 @@
 import express from "express";
-import { signup, login } from "../controllers/auth.controller.js";
+import { signup, login, dashboard, taskList } from "../controllers/auth.controller.js";
 import { checkauth } from "../middleware/auth.middleware.js";
-import { dashboard } from "../controllers/auth.controller.js";
 
 
 const router = express.Router();
@@ -13,6 +12,7 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/dashboard", checkauth, dashboard);
+router.get("/task-list", checkauth, taskList);
 
 
 export default router;
