3c5399a855bb52dbb2ebd6008421cea25d7e0378
Rikita Roy <rikitaroy21@gmail.com>
2026-04-01 15:48:36 +0530

    add pending_to_completed task route

--- backend/src/routes/task.route.js
+++ backend/src/routes/task.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { new_task} from "../controllers/task.controller.js";
+import { new_task, pending_to_completed } from "../controllers/task.controller.js";
 import { checkauth } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/new_task", checkauth, new_task);
+router.post("/pending_to_completed", checkauth, pending_to_completed);
 
 export default router;
