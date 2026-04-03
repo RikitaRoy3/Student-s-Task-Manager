f528504cca82104253f1b06c8a1664db6bcd2e20
Rikita Roy <rikitaroy21@gmail.com>
2026-04-03 11:44:15 +0530

    changing post request to put request

--- backend/src/routes/task.route.js
+++ backend/src/routes/task.route.js
@@ -5,6 +5,6 @@ import { checkauth } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post("/new_task", checkauth, new_task);
-router.post("/pending_to_completed", checkauth, pending_to_completed);
+router.put("/pending_to_completed", checkauth, pending_to_completed);
 
 export default router;
