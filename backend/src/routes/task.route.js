e0492e8da20dc3746c20f75229692a3bcd20e72d
Rikita Roy <rikitaroy21@gmail.com>
2026-04-03 11:48:27 +0530

    add route for the controller just now added

--- backend/src/routes/task.route.js
+++ backend/src/routes/task.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { new_task, pending_to_completed } from "../controllers/task.controller.js";
+import { new_task, pending_to_completed, completed_to_pending } from "../controllers/task.controller.js";
 import { checkauth } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/new_task", checkauth, new_task);
 router.put("/pending_to_completed", checkauth, pending_to_completed);
+router.put("/completed_to_pending", checkauth, completed_to_pending);
 
 export default router;
