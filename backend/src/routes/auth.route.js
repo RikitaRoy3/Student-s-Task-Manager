8b23f304b1a8208ffc4b3036729a1451b1954e4c
Rikita Roy <rikitaroy21@gmail.com>
2026-04-04 12:17:54 +0530

    add route for the avatar controller

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, login, dashboard, taskList, profile,logout } from "../controllers/auth.controller.js";
+import { signup, login, dashboard, taskList, profile,logout, avatar } from "../controllers/auth.controller.js";
 import { checkauth } from "../middleware/auth.middleware.js";
 
 
@@ -15,6 +15,7 @@ router.post("/logout", checkauth, logout);
 router.get("/dashboard", checkauth, dashboard);
 router.get("/tasklist", checkauth, taskList);
 router.get("/profile", checkauth, profile);
+router.get("/avatar", checkauth, avatar);
 
 
 export default router;
