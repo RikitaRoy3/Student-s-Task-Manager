981a6324008451df3f5b3c98e1282492343e0ceb
Rikita Roy <rikitaroy21@gmail.com>
2026-04-06 18:35:29 +0530

    new file added to check authenticated user in the frontend

---
+++ frontend/vite-project/src/Component/ProtectedRoute.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { Navigate } from "react-router";
+
+function ProtectedRoute({ children }) {
+
+  const [isAuth, setIsAuth] = useState(null);
+
+  useEffect(() => {
+    authCheck();
+  }, []);
+
+  const authCheck = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/api/auth/auth-check", {
+        method: "GET",
+        credentials: "include",
+      });
+
+      if (res.ok) {
+        setIsAuth(true);
+      } else {
+        setIsAuth(false);
+      }
+
+    } catch (error) {
+      setIsAuth(false);
+    }
+  };
+
+  if (isAuth === null) return <p>Loading...</p>;
+
+  if (!isAuth) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+}
+
+export default ProtectedRoute;
