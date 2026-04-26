d041bb5a81709e488e5f7d6cd824aacc4c25320d
RikitaRoy3 <rikitaroy2005@gmail.com>
2026-04-26 08:50:05 +0530

    add route of the delete task controller

--- backend/src/routes/task.route.js
+++ backend/src/routes/task.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { new_task, pending_to_completed, completed_to_pending } from "../controllers/task.controller.js";
+import { new_task, pending_to_completed, completed_to_pending, delete_task } from "../controllers/task.controller.js";
 import { checkauth } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/new_task", checkauth, new_task);
 router.put("/pending_to_completed", checkauth, pending_to_completed);
 router.put("/completed_to_pending", checkauth, completed_to_pending);
+router.put("/delete_task", checkauth, delete_task);
 
 export default router;
